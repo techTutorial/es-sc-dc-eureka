33910ce99610471ea2e8a03a9796ea7faf29712e
tech Tutorial <talk.to.amit.verma@gmail.com>
2020-05-03 01:33:39 +0530

    updates > working

--- src/main/java/com/eurekaclient/WelcomeApp.java
+++ src/main/java/com/eurekaclient/WelcomeApp.java
@@ -3,10 +3,12 @@ package com.eurekaclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 // Below annotation is used to list down the application on the eureka server (i.e. the registry server).
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
+@EnableEurekaClient
 public class WelcomeApp {
 
 	public static void main(String[] args) {
