8449fa9ffed941b5f4f6ff5366805877fbb39c13
tech Tutorial <talk.to.amit.verma@gmail.com>
2020-05-20 14:29:32 +0530

    updates > working

---
+++ src/main/java/example/spring/cloud/eureka/config/RibbonConfiguration.java
@@ -0,0 +1,18 @@
+package example.spring.cloud.eureka.config;
+
+import org.springframework.context.annotation.Bean;
+import com.netflix.loadbalancer.IPing;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.NoOpPing;
+import com.netflix.loadbalancer.WeightedResponseTimeRule;
+
+public class RibbonConfiguration {
+     @Bean
+     public IPing ribbonPing() {
+            return new NoOpPing();
+     }   
+     @Bean  
+     public IRule ribbonRule() {
+            return new WeightedResponseTimeRule();
+     }
+}
--- src/main/java/example/spring/cloud/eureka/service/impl/GreetingsServiceImpl.java
+++ src/main/java/example/spring/cloud/eureka/service/impl/GreetingsServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import example.spring.cloud.eureka.service.GreetingsService;
@@ -23,12 +24,21 @@ public class GreetingsServiceImpl implements GreetingsService {
 		greetingsMap.put("en", "GOOD MORNING");
 	}
 	
+	
+	@Value("${eureka.instance.instance-id}")
+	private String instanceId;
+	
+	
 	@Override
 	public String getGreetings(String langCode) {
 		System.out.println("Fetching greetings type for locale id= " + langCode);
 		String msg = greetingsMap.entrySet().stream().filter((lId) -> langCode.equalsIgnoreCase(lId.getKey()))
 				.map(langName -> langName.getValue()).collect(Collectors.joining());
-		return msg;
+		
+		// Useful to verify multiple instances of micro-service by clicking on refresh button of browser
+		String msgInst = msg + " using instance id " + instanceId;
+		
+		return msgInst;
 	}
 
 }
